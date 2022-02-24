a9c26d19e2ad1b31f51b2336f7d8ad89dc0c47d8
pa20172019 <97398154+pa20172019@users.noreply.github.com>
2022-02-24 15:59:00 -0600

    changed code

--- src/main/java/addNewCustomer/GitTest2.java
+++ src/main/java/addNewCustomer/GitTest2.java
@@ -3,8 +3,12 @@ package addNewCustomer;
 public class GitTest2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 System.out.println("git test2");
+		 printme();
 	}
-
+   public static void printme() {
+	   System.out.println("git 1");
+	   System.out.println("git 2");
+	   
+   }
 }
